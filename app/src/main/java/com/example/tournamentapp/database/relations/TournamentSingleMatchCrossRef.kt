64ac2b0c302a227807824fddf0d874f04305a188
Dawid Siudeja <dawid.arsyl@gmail.com>
2023-04-26 21:55:13 +0200

    delete nousing files in relations package

--- app/src/main/java/com/example/tournamentapp/database/relations/TournamentSingleMatchCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tournamentapp.database.relations
-
-import androidx.room.Entity
-
-@Entity(primaryKeys = ["tournamentTitle", "matchId"])
-data class TournamentSingleMatchCrossRef(
-    val tournamentTitle: String,
-    val matchId: String
-)
